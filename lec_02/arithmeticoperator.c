13f731796d99b8ec5c5e9d975231dbcfd8423457
Prashant-Yadav <prashant.cs241244@global.org.in>
2025-09-29 09:24:55 +0530

    second commit

---
+++ lec_02/arithmeticoperator.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int x = 10, y = 3;
+    printf("x + y = %d\n", x + y);   
+    printf("x - y = %d\n", x - y);   
+    printf("x * y = %d\n", x * y);   
+    printf("x / y = %d\n", x / y);   
+    printf("x %% y = %d\n\n", x % y); 
+    return 0;
+}
